2f6bafa8726693fdea797e872832cba8343b082b
Ismail Bin Mujeeb <ismailbinmujeeb.com>
2025-12-15 15:38:46 +0530

    feat: add novel model

---
+++ server/models/novel.model.ts
@@ -0,0 +1,17 @@
+import mongoose, { Document } from "mongoose";
+
+export interface INovel extends Document {
+  title: string;
+  userId: mongoose.ObjectId;
+  plot: string;
+  coverPrompt: string;
+}
+
+const novelSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  plot: { type: String, required: true },
+  coverPrompt: { type: String, required: true },
+});
+
+export const Novel = mongoose.model<INovel>("Novel", novelSchema);
