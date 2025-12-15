d2459279ecf88742446818f6f47545adfa6723e7
Ismail Bin Mujeeb <ismailbinmujeeb.com>
2025-12-15 15:53:41 +0530

    feat: add form model

---
+++ server/models/form.model.ts
@@ -0,0 +1,32 @@
+import mongoose, { Document } from "mongoose";
+
+export interface IFormField {
+  label: string;
+  answer: string | null;
+  level: number;
+  isAnswered: boolean;
+}
+
+export interface IForm extends Document {
+  novelId: mongoose.Schema.Types.ObjectId;
+  fields: IFormField[];
+}
+
+export const FormSchema = new mongoose.Schema({
+  novelId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Novel",
+    required: true,
+    unique: true,
+  },
+  fields: [
+    {
+      label: { type: String, required: true },
+      answer: { type: String, default: null },
+      level: { type: Number, required: true },
+      isAnswered: { type: Boolean, default: false },
+    },
+  ],
+});
+
+export const Form = mongoose.model<IForm>("Form", FormSchema);
